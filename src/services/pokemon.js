776ab1105c487681105a28e5330a9226ea1dbc52
Bo Opfer <bo_opfer_ars@whirlpool.com>
2021-11-19 13:19:30 +0700

    working with check on fetch

--- src/services/pokemon.js
+++ src/services/pokemon.js
@@ -1,10 +1,23 @@
 // Need to use the React-specific entry point to import createApi
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
+
+const baseQuery = fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' })
+const baseQueryTest = async (args, api, extraOptions) => {
+  let result = await baseQuery(args, api, extraOptions)
+  if (result.error && result.error.status === 401) {
+
+  } else {
+    console.log("working still?")
+    return result
+  }
+}
+
+
 // Define a service using a base URL and expected endpoints
 export const pokemonApi = createApi({
   reducerPath: 'pokemonApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://pokeapi.co/api/v2/' }),
+  baseQuery: baseQueryTest,
   endpoints: (builder) => ({
     getPokemonByName: builder.query({
       query: (name) => `pokemon/${name}`,
